653bae088b754811f70601da4f79187cbab37e01
dwyerb64 <dwyerb@tcd.ie>
2016-11-10 22:42:52 +0000

    Remove footer text

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bigo' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bigo' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bigo' ), 'bigo', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
