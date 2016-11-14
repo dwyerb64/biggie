c7e11f3d50285d3076a6254c0226a11320b0f967
bmpconnolly <bcconnolly@gmail.com>
2016-11-14 02:01:48 +0000

    Added Work unit area for inside page & AVF's

--- page_inside.php
+++ page_inside.php
@@ -1,7 +1,10 @@
 <?php
 /* Template name: Inside Page */
-
 get_header(); ?>
+
+<?php 
+$work_unit_image = get_field('work_unit_image');
+ ?>
 
 <section id="featured-image" class="container">
 <?php
@@ -10,6 +13,21 @@ if ( has_post_thumbnail() ) {
 } 
 the_content();
 ?>
+</section>
+
+<section class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<h2><?php the_field('work_unit_title'); ?></h2>
+			<figure>
+				<img src="<?php echo $work_unit_image['url']; ?>" alt="<?php echo $work_unit_image['alt']; ?>" />
+				<figcaption><?php the_field('work_unit_image_caption'); ?></figcaption>
+			<figure>
+		</div>
+		<div class="col-md-6">
+			<p><?php the_field('work_unit_text_area'); ?></p>
+		</div>
+	</div>
 </section>
 
 <?php get_header(); ?>
--- plugins/advanced-custom-fields/custom-fields-data.php
+++ plugins/advanced-custom-fields/custom-fields-data.php
@@ -1,22 +1,9 @@
-<?php
 if(function_exists("register_field_group"))
 {
 	register_field_group(array (
 		'id' => 'acf_about-us-details',
 		'title' => 'About Us Details',
 		'fields' => array (
-			array (
-				'key' => 'field_58265bb10cb13',
-				'label' => 'About Us Title',
-				'name' => 'about_us_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
 			array (
 				'key' => 'field_58265c2c0cb14',
 				'label' => 'About Us Details',
@@ -53,6 +40,17 @@ if(function_exists("register_field_group"))
 		'title' => 'Contact Details',
 		'fields' => array (
 			array (
+				'key' => 'field_580c88cd84e6c',
+				'label' => 'Contact Details Title',
+				'name' => 'contact_details_title',
+				'type' => 'textarea',
+				'default_value' => '',
+				'placeholder' => '',
+				'maxlength' => '',
+				'rows' => '',
+				'formatting' => 'br',
+			),
+			array (
 				'key' => 'field_580c895e84e6d',
 				'label' => 'Contact Details Content',
 				'name' => 'contact_details_content',
@@ -84,9 +82,89 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 	register_field_group(array (
+		'id' => 'acf_inside-page-workunit',
+		'title' => 'Inside Page - WorkUnit',
+		'fields' => array (
+			array (
+				'key' => 'field_582908396b7a7',
+				'label' => 'work unit title',
+				'name' => 'work_unit_title',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_582909883cbbe',
+				'label' => 'work unit image',
+				'name' => 'work_unit_image',
+				'type' => 'image',
+				'save_format' => 'object',
+				'preview_size' => 'thumbnail',
+				'library' => 'all',
+			),
+			array (
+				'key' => 'field_58290a4369a72',
+				'label' => 'work unit image caption',
+				'name' => 'work_unit_image_caption',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_58290ac069a73',
+				'label' => 'work unit text area',
+				'name' => 'work_unit_text_area',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page_inside.php',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
 		'id' => 'acf_work-item',
 		'title' => 'Work Item',
 		'fields' => array (
+			array (
+				'key' => 'field_58224daff0d91',
+				'label' => 'Client',
+				'name' => 'client',
+				'type' => 'text',
+				'instructions' => 'Enter Client Name',
+				'required' => 1,
+				'default_value' => 'Mondello',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'none',
+				'maxlength' => '',
+			),
 			array (
 				'key' => 'field_58224fc19580d',
 				'label' => 'Image Section 1',
@@ -109,6 +187,16 @@ if(function_exists("register_field_group"))
 				'layout' => 'table',
 				'button_label' => 'Add Image',
 			),
+			array (
+				'key' => 'field_58224e1df0d94',
+				'label' => 'Body Part 1',
+				'name' => 'body_part_1',
+				'type' => 'wysiwyg',
+				'instructions' => 'The first part of the article goes here. This section will be right aligned.',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
 		),
 		'location' => array (
 			array (
@@ -130,4 +218,3 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 }
-?>
