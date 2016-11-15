57dbe8fe32992adb75141273cd3de1cb72a3b644
bmpconnolly <bcconnolly@gmail.com>
2016-11-15 22:43:18 +0000

    Update portfolio ACF

--- plugins/advanced-custom-fields/custom-fields-data.php
+++ plugins/advanced-custom-fields/custom-fields-data.php
@@ -6,18 +6,6 @@ if(function_exists("register_field_group"))
 		'title' => 'About Us Details',
 		'fields' => array (
 			array (
-				'key' => 'field_58265bb10cb13',
-				'label' => 'About Us Title',
-				'name' => 'about_us_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
 				'key' => 'field_58265c2c0cb14',
 				'label' => 'About Us Details',
 				'name' => 'about_us_details',
@@ -53,6 +41,17 @@ if(function_exists("register_field_group"))
 		'title' => 'Contact Details',
 		'fields' => array (
 			array (
+				'key' => 'field_580c88cd84e6c',
+				'label' => 'Contact Details Title',
+				'name' => 'contact_details_title',
+				'type' => 'textarea',
+				'default_value' => '',
+				'placeholder' => '',
+				'maxlength' => '',
+				'rows' => '',
+				'formatting' => 'br',
+			),
+			array (
 				'key' => 'field_580c895e84e6d',
 				'label' => 'Contact Details Content',
 				'name' => 'contact_details_content',
@@ -79,6 +78,7 @@ if(function_exists("register_field_group"))
 			'position' => 'normal',
 			'layout' => 'no_box',
 			'hide_on_screen' => array (
+				0 => 'the_content',
 			),
 		),
 		'menu_order' => 0,
@@ -88,6 +88,18 @@ if(function_exists("register_field_group"))
 		'title' => 'Inside Page - WorkUnit',
 		'fields' => array (
 			array (
+				'key' => 'field_582908396b7a7',
+				'label' => 'work unit title',
+				'name' => 'work_unit_title',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
 				'key' => 'field_58290ac069a73',
 				'label' => 'work unit text area',
 				'name' => 'work_unit_text_area',
@@ -96,13 +108,25 @@ if(function_exists("register_field_group"))
 				'toolbar' => 'full',
 				'media_upload' => 'yes',
 			),
+			array (
+				'key' => 'field_58290a4369a72',
+				'label' => 'work unit image caption',
+				'name' => 'work_unit_image_caption',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
 		),
 		'location' => array (
 			array (
 				array (
-					'param' => 'page_template',
+					'param' => 'post_type',
 					'operator' => '==',
-					'value' => 'page_inside.php',
+					'value' => 'portfolio',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
@@ -121,6 +145,20 @@ if(function_exists("register_field_group"))
 		'title' => 'Work Item',
 		'fields' => array (
 			array (
+				'key' => 'field_58224daff0d91',
+				'label' => 'Client',
+				'name' => 'client',
+				'type' => 'text',
+				'instructions' => 'Enter Client Name',
+				'required' => 1,
+				'default_value' => 'Mondello',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'none',
+				'maxlength' => '',
+			),
+			array (
 				'key' => 'field_58224e1df0d94',
 				'label' => 'Body Part 1',
 				'name' => 'body_part_1',
@@ -129,6 +167,28 @@ if(function_exists("register_field_group"))
 				'default_value' => '',
 				'toolbar' => 'full',
 				'media_upload' => 'yes',
+			),
+			array (
+				'key' => 'field_58224fc19580d',
+				'label' => 'Image Section 1',
+				'name' => 'image_section_1',
+				'type' => 'repeater',
+				'sub_fields' => array (
+					array (
+						'key' => 'field_58224ffd22b27',
+						'label' => 'Image',
+						'name' => 'image',
+						'type' => 'image',
+						'column_width' => '',
+						'save_format' => 'object',
+						'preview_size' => 'medium',
+						'library' => 'all',
+					),
+				),
+				'row_min' => 1,
+				'row_limit' => 3,
+				'layout' => 'table',
+				'button_label' => 'Add Image',
 			),
 		),
 		'location' => array (
@@ -151,6 +211,7 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 }
+
 
 
 ?>
--- single-portfolio.php
+++ single-portfolio.php
@@ -13,13 +13,33 @@ get_header(); ?>
 
 
     <?php  while ( have_posts() ) : the_post(); ?>
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-      <img src="http://placehold.it/350x150" alt="">
-    </div>
-  </div>
-</div>
+		<?php 
+		$work_unit_image = get_field('work_unit_image');
+		 ?>
+
+		<section id="featured-image" class="container">
+		<?php
+		if ( has_post_thumbnail() ) {
+			the_post_thumbnail('full', array( 'class' => 'img-responsive' ));
+		} 
+		the_content();
+		?>
+		</section>
+
+		<section class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2><?php the_field('work_unit_title'); ?></h2>
+					<figure>
+						<img src="<?php echo $work_unit_image['url']; ?>" alt="<?php echo $work_unit_image['alt']; ?>" />
+						<figcaption><?php the_field('work_unit_image_caption'); ?></figcaption>
+					<figure>
+				</div>
+				<div class="col-md-6">
+					<p><?php the_field('work_unit_text_area'); ?></p>
+				</div>
+			</div>
+		</section>
 
     <?php endwhile; // End of the loop. ?>
 
