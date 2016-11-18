a91a31c909efa4532363490af5f8659e97b1920f
bmpconnolly <bcconnolly@gmail.com>
2016-11-18 00:59:33 +0000

    Add footer start

--- footer.php
+++ footer.php
@@ -13,9 +13,22 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			
+	<footer id="colophon" class=" container site-footer" role="contentinfo">
+		<div class="row site-info">
+			<div class="col-md-6">
+				<p>
+					big O <br>									
+					9 Mount Street Crescent <br>
+					Dublin 2 <br>
+					Ireland<br>
+					tel. +353 (1) 531 255<br>
+					email: hi@bigo.ie<br>
+
+				</p>
+			</div>
+			<div class="col-md-6">
+				<!-- ICONS --><i class="fa fa-camera-retro"></i>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
