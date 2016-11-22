dcb1b7b5f01fc7db30eb14b0abcf8942660ae3df
dwyerb64 <dwyerb@tcd.ie>
2016-11-22 22:03:33 +0000

    Add advanced custom fields pro

--- functions.php
+++ functions.php
@@ -218,40 +218,46 @@ require get_template_directory() . '/inc/jetpack.php';
 
 
 
-// // 1. customize ACF path
-// add_filter('acf/settings/path', 'my_acf_settings_path');
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
  
-// function my_acf_settings_path( $path ) {
+function my_acf_settings_path( $path ) {
  
-//     // update path
-//     $path = get_stylesheet_directory() . '/plugins/advanced-custom-fields/';
+    // update path
+    $path = get_stylesheet_directory() . '/plugins/advanced-custom-fields-pro/';
     
-//     // return
-//     return $path;
+    // return
+    return $path;
     
-// }
+}
  
 
-// // 2. customize ACF dir
-// add_filter('acf/settings/dir', 'my_acf_settings_dir');
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
  
-// function my_acf_settings_dir( $dir ) {
+function my_acf_settings_dir( $dir ) {
  
-//     // update path
-//     $dir = get_stylesheet_directory_uri() . '/plugins/advanced-custom-fields/';
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/plugins/advanced-custom-fields-pro/';
     
-//     // return
-//     return $dir;
+    // return
+    return $dir;
     
-// }
+}
  
 
-// // 3. Hide ACF field group menu item
-// // add_filter('acf/settings/show_admin', '__return_false');
+// 3. Hide ACF field group menu item
+// add_filter('acf/settings/show_admin', '__return_false');
 
 
-// // 4. Include ACF
-// include_once( get_stylesheet_directory() . '/plugins/advanced-custom-fields/acf.php' );
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/plugins/advanced-custom-fields-pro/acf.php' );
 
-require_once(get_stylesheet_directory() . '/plugins/advanced-custom-fields/custom-fields-data.php');
-require get_template_directory() .'/plugins/acf-repeater/acf-repeater.php';
+// require_once(get_stylesheet_directory() . '/plugins/advanced-custom-fields/custom-fields-data.php');
+// require get_template_directory() .'/plugins/acf-repeater/acf-repeater.php';
+
+
+
+
+
+
