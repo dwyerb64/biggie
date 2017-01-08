47bedbe6437be2ecbc2b56da400105d44c15f195
bmpconnolly <bcconnolly@gmail.com>
2017-01-08 20:43:07 +0000

    PLay button changed for video

--- template-parts/partial-video.php
+++ template-parts/partial-video.php
@@ -13,9 +13,8 @@
         <img class="portfolio-placeholder" src="<?php echo $placeholderImage['url']; ?>" alt="<?php echo $placeholderImage['alt']; ?>" />
 
       <?php endif; ?>
-      <span class="play-button" style="border-color: <?php echo the_sub_field('play_button_colour') ?>">
-        <i class="fa fa-4x fa-chevron-right js-play-video" style="color: <?php echo the_sub_field('play_button_colour') ?>; " aria-hidden="true"></i>
-      </span>
+     
+      <div class="play-button play-button-img"></div>
     </div>
   </div>
 </div>
