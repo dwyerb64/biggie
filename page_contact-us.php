0fc1f27cec130141fba0c75c777bd7d29f09507c
bmpconnolly <bcconnolly@gmail.com>
2017-01-08 17:35:06 +0000

    Added std footer to contact-us to match zeplin. Ajusted positioning to zeplin doc

--- page_contact-us.php
+++ page_contact-us.php
@@ -8,17 +8,10 @@ get_header(); ?>
 		<div class="title-block col-md-6" >
 			<h1><?php the_field('contact_details_title'); ?></h1>
 		</div>
-		<div class="content-block col-md-6">
-			<div class="wrapper">
-				<p class="contact-name hidden-xs"><?php the_field('company_name', 'option'); ?></p>
-				<p class="contact-address"><?php the_field('address_line_1', 'option'); ?></p>
-				<p class="contact-address"><?php the_field('address_line_2', 'option'); ?></p>
-				<p class="contact-address"><?php the_field('country', 'option'); ?></p>
-				<p class="contact-info"><strong>tel. </strong><?php the_field('telephone_number', 'option'); ?></p>
-			</div>	
-		</div>
 	</div>	
 </section>
 
+<?php
+get_footer();
 
 
