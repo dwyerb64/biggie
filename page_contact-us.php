fbb2d3f8ef713194234623f8716a728181ec4d59
bmpconnolly <bcconnolly@gmail.com>
2017-01-25 22:29:08 +0000

    Added mailto link to contact-us page title.

--- page_contact-us.php
+++ page_contact-us.php
@@ -6,7 +6,7 @@ get_header(); ?>
 <section id="contact-us" class="container" >
 	<div class="row">
 		<div class="title-block col-md-6" >
-			<h1><?php the_field('contact_details_title'); ?></h1>
+			<h1><a href="mailto:<?php the_field('email_address', 'option');?>"><?php the_field('contact_details_title'); ?></a></h1>
 		</div>
 	</div>	
 </section>
